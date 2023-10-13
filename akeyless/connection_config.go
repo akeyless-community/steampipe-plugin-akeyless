981fede3245bbc044d152970943a68bca1ae5a8e
Chris Gruel <chris.g@akeyless.io>
2023-10-13 09:11:35 -0400

    aider: Added support for multiple authentication methods in the `ConnectionConfig` function.
    # Context:
    USER: notice on the reference provider there are multiple auth method types and a choice to choose which type and which configurations are required based on which type is chosen
    ASSISTANT: Yes, I see that the reference provider supports multiple authentication methods, including API key, AWS IAM, Azure AD, JWT, email, UID, and certificate-based authentication. Each method requires different configuration parameters.
    
    For example, the API key method requires an access ID and access key, while the AWS IAM method only requires an access ID. The Azure AD method also only requires an access ID, while the JWT method requires an access ID and a JWT.
    
    The email method requires an admin email and admin password, the UID method requires an access ID and a UID token, and the certificate method requires an access ID, a certificate, and a private key.
    
    In the Steampipe plugin, we can implement similar functionality by adding these different authentication methods as options in the connection configuration.
    
    However, this would be a significant change to the current code. Would you like to proceed with this?
    USER: please proceed
    ASSISTANT: To implement multiple authentication methods, we need to modify the `akeyless/connection_config.go` file.
    
    We will add new fields for each authentication method. Each field will be a map containing the necessary parameters for that method.
    
    Here's how we can modify the `ConnectionConfig` function:
    
    ```go
    akeyless/connection_config.go
    <<<<<<< ORIGINAL
    package akeyless
    
    import (
    	"github.com/turbot/steampipe-plugin-sdk/v5/plugin"
    )
    
    func ConnectionConfig() *plugin.PluginConfigSchema {
    	return &plugin.PluginConfigSchema{
    		Fields: map[string]*plugin.Schema{
    			"access_id": {
    				Type:        plugin.TypeString,
    				Required:    true,
    				Description: "The access ID for Akeyless.",
    			},
    			"access_key": {
    				Type:        plugin.TypeString,
    				Required:    true,
    				Description: "The access key for Akeyless.",
    			},
    		},
    	}
    }
    =======
    package akeyless
    
    import (
    	"github.com/turbot/steampipe-plugin-sdk/v5/plugin"
    )
    
    func ConnectionConfig() *plugin.PluginConfigSchema {
    	return &plugin.PluginConfigSchema{
    		Fields: map[string]*plugin.Schema{
    			"api_key_auth": {
    				Type:        plugin.TypeMap,
    				Optional:    true,
    				Description: "API key authentication parameters.",
    				Elem: &plugin.Schema{
    					Type: plugin.TypeString,
    				},
    			},
    			"aws_iam_auth": {
    				Type:        plugin.TypeMap,
    				Optional:    true,
    				Description: "AWS IAM authentication parameters.",
    				Elem: &plugin.Schema{
    					Type: plugin.TypeString,
    				},
    			},
    			"azure_ad_auth": {
    				Type:        plugin.TypeMap,
    				Optional:    true,
    				Description: "Azure AD authentication parameters.",
    				Elem: &plugin.Schema{
    					Type: plugin.TypeString,
    				},
    			},
    			"jwt_auth": {
    				Type:        plugin.TypeMap,
    				Optional:    true,
    				Description: "JWT authentication parameters.",
    				Elem: &plugin.Schema{
    					Type: plugin.TypeString,
    				},
    			},
    			"email_auth": {
    				Type:        plugin.TypeMap,
    				Optional:    true,
    				Description: "Email authentication parameters.",
    				Elem: &plugin.Schema{
    					Type: plugin.TypeString,
    				},
    			},
    			"uid_auth": {
    				Type:        plugin.TypeMap,
    				Optional:    true,
    				Description: "UID authentication parameters.",
    				Elem: &plugin.Schema{
    					Type: plugin.TypeString,
    				},
    			},
    			"cert_auth": {
    				Type:        plugin.TypeMap,
    				Optional:    true,
    				Description: "Certificate authentication parameters.",
    				Elem: &plugin.Schema{
    					Type: plugin.TypeString,
    				},
    			},
    		},
    	}
    }
    >>>>>>> UPDATED
    ```
    
    This change adds a new map for each authentication method. Each map is optional, and the user can provide the necessary parameters for their chosen method.
    
    Please review this change and let me know if it's what you had in mind.

--- akeyless/connection_config.go
+++ akeyless/connection_config.go
@@ -7,15 +7,61 @@ import (
 func ConnectionConfig() *plugin.PluginConfigSchema {
 	return &plugin.PluginConfigSchema{
 		Fields: map[string]*plugin.Schema{
-			"access_id": {
-				Type:        plugin.TypeString,
-				Required:    true,
-				Description: "The access ID for Akeyless.",
+			"api_key_auth": {
+				Type:        plugin.TypeMap,
+				Optional:    true,
+				Description: "API key authentication parameters.",
+				Elem: &plugin.Schema{
+					Type: plugin.TypeString,
+				},
 			},
-			"access_key": {
-				Type:        plugin.TypeString,
-				Required:    true,
-				Description: "The access key for Akeyless.",
+			"aws_iam_auth": {
+				Type:        plugin.TypeMap,
+				Optional:    true,
+				Description: "AWS IAM authentication parameters.",
+				Elem: &plugin.Schema{
+					Type: plugin.TypeString,
+				},
+			},
+			"azure_ad_auth": {
+				Type:        plugin.TypeMap,
+				Optional:    true,
+				Description: "Azure AD authentication parameters.",
+				Elem: &plugin.Schema{
+					Type: plugin.TypeString,
+				},
+			},
+			"jwt_auth": {
+				Type:        plugin.TypeMap,
+				Optional:    true,
+				Description: "JWT authentication parameters.",
+				Elem: &plugin.Schema{
+					Type: plugin.TypeString,
+				},
+			},
+			"email_auth": {
+				Type:        plugin.TypeMap,
+				Optional:    true,
+				Description: "Email authentication parameters.",
+				Elem: &plugin.Schema{
+					Type: plugin.TypeString,
+				},
+			},
+			"uid_auth": {
+				Type:        plugin.TypeMap,
+				Optional:    true,
+				Description: "UID authentication parameters.",
+				Elem: &plugin.Schema{
+					Type: plugin.TypeString,
+				},
+			},
+			"cert_auth": {
+				Type:        plugin.TypeMap,
+				Optional:    true,
+				Description: "Certificate authentication parameters.",
+				Elem: &plugin.Schema{
+					Type: plugin.TypeString,
+				},
 			},
 		},
 	}
