82da2476d7e000e31513daa51912ab13081573f8
peter-akeyless <peter.t@akeyless.io>
2024-06-26 13:08:27 +0300

    support versioning from goreleaser

--- main.go
+++ main.go
@@ -5,6 +5,14 @@ import (
 	"github.com/turbot/steampipe-plugin-sdk/v5/plugin"
 )
 
+// The version injected by goreleaser
+// https://goreleaser.com/cookbooks/using-main.version/
+
+var (
+	version = "0.0.0-dev"
+)
+
 func main() {
+	akeyless.PluginVersion = version
 	plugin.Serve(&plugin.ServeOpts{PluginFunc: akeyless.Plugin, PluginName: akeyless.PluginName})
 }
